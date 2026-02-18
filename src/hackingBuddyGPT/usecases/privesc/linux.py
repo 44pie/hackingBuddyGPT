a58e97870566450649db1d4f460f355f5f9ce492
44pie <fleshy-mango-pogo@duck.com>
2026-02-18 11:03:32 +0000

    Add gsocket connection support to LinuxPrivesc use case

--- src/hackingBuddyGPT/usecases/privesc/linux.py
+++ src/hackingBuddyGPT/usecases/privesc/linux.py
@@ -1,14 +1,15 @@
-from hackingBuddyGPT.capabilities import SSHRunCommand, SSHTestCredential
+from hackingBuddyGPT.capabilities import SSHRunCommand, SSHTestCredential, GsocketRunCommand, GsocketTestCredential
 from hackingBuddyGPT.capabilities.local_shell import LocalShellCapability
 from hackingBuddyGPT.usecases.base import AutonomousAgentUseCase, use_case
 from hackingBuddyGPT.utils import SSHConnection
 from hackingBuddyGPT.utils.local_shell import LocalShellConnection
+from hackingBuddyGPT.utils.gsocket_connection import GsocketConnection
 from typing import Union
 from .common import Privesc
 
 
 class LinuxPrivesc(Privesc):
-    conn: Union[SSHConnection, LocalShellConnection] = None
+    conn: Union[SSHConnection, LocalShellConnection, GsocketConnection] = None
     system: str = "linux"
 
     def init(self):
@@ -16,6 +17,9 @@ class LinuxPrivesc(Privesc):
         if isinstance(self.conn, LocalShellConnection):
             self.add_capability(LocalShellCapability(conn=self.conn), default=True)
             self.add_capability(SSHTestCredential(conn=self.conn))
+        elif isinstance(self.conn, GsocketConnection):
+            self.add_capability(GsocketRunCommand(conn=self.conn), default=True)
+            self.add_capability(GsocketTestCredential(conn=self.conn))
         else:
             self.add_capability(SSHRunCommand(conn=self.conn), default=True)
             self.add_capability(SSHTestCredential(conn=self.conn))
