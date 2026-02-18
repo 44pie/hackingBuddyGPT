4cf3bb1a12e637ae02961253b511f4e12abd0907
44pie <fleshy-mango-pogo@duck.com>
2026-02-18 11:23:36 +0000

    Fix: export GsocketRunCommand and GsocketTestCredential from capabilities __init__

--- src/hackingBuddyGPT/capabilities/__init__.py
+++ src/hackingBuddyGPT/capabilities/__init__.py
@@ -3,6 +3,8 @@ from .psexec_run_command import PSExecRunCommand
 from .psexec_test_credential import PSExecTestCredential
 from .ssh_run_command import SSHRunCommand
 from .ssh_test_credential import SSHTestCredential
+from .gsocket_run_command import GsocketRunCommand
+from .gsocket_test_credential import GsocketTestCredential
 
 __all__ = [
     "Capability",
@@ -10,4 +12,6 @@ __all__ = [
     "PSExecTestCredential",
     "SSHRunCommand",
     "SSHTestCredential",
+    "GsocketRunCommand",
+    "GsocketTestCredential",
 ]
